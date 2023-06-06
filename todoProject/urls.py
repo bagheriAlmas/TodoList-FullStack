e2611e886e072b2acbcd0051a3e277350d7801f7
mahdibagheri <bagheri.almas@gmail.com>
2023-06-06 22:12:51 +0330

    add jwt to project

--- todoProject/urls.py
+++ todoProject/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from todoProject.views import UserRegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/register', UserRegisterView.as_view(), name='user-register'),
-    path('', include('tasks.urls')),
+    path('api/', include('tasks.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
