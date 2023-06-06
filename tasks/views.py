bf6d0a185d2e880ed2093d863e7db455b547d0c3
mahdibagheri <bagheri.almas@gmail.com>
2023-06-06 22:21:40 +0330

    update TaskView that each user can see own tasks

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,13 @@ from rest_framework import permissions
 from .models import Task
 from .serializers import TaskSerializer
 
+
 class TaskView(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
