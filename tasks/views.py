e0561b1396f0908fbe9f04e9b0b1b3a9467067cd
mahdibagheri <bagheri.almas@gmail.com>
2023-06-06 22:13:45 +0330

    add permission for TaskView view

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import Task
 from .serializers import TaskSerializer
-
 
 class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
